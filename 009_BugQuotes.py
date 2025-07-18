d145bc7ecf3ce25d7f0637ab02d34ed2772d7f0c
Cean <minchiseven@gmail.com>
2025-07-18 16:49:09 +0800

    Bug Quotes

---
+++ 009_BugQuotes.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    " Do what you can, with what you have, where you are. - Theodore Roosevelt",
+    "Keep your face always toward the sunshine and shadows will fall behind you. - Walt Whitman",
+    "A smooth sea never made a skilled sailor. - Franklin D. Roosevelt",
+    "Even the darkest night will end and the sun will rise. - Victor Hugo",
+    "It is not the strongest of the species that survive, but the one most responsive to change . -Charles Darwin"
+]
+
+print("random quote of the day buggy:\n")
+print(random.choice(quotes))
