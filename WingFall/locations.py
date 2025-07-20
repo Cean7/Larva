29e02ba203d1cddb20672c47e7c26896d08abfea
Cean <minchiseven@gmail.com>
2025-07-20 11:15:29 +0800

    Game Locations

---
+++ WingFall/locations.py
@@ -0,0 +1,37 @@
+# locations
+
+liriath_grove = {
+    "name": "Liriath Grove",
+    "description": "null",
+    "type": "Home",
+    "connected_to": ["River Breeze Path", "Hollow Route"]
+}
+
+river_breeze_path = {
+    "name": "River Breeze Path",
+    "description": "null",
+    "type": "Travel Route",
+    "connected_to": ["Petal Spire"]
+}
+
+hollow_route = {
+    "name": "Hollow Route",
+    "description": "null",
+    "type": "Travel Route",
+    "connected_to": ["Petal Spire"]
+}
+
+petal_spire = {
+    "name": "Petal Spire",
+    "description": "null",
+    "type": "Destination",
+    "connected_to": []
+}
+
+
+all_locations = [
+    liriath_grove,
+    river_breeze_path,
+    hollow_route,
+    petal_spire
+]
