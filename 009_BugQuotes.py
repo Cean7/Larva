12c34ff0ee1aa3037a7c912a681e52161bb2c02d
Cean <minchiseven@gmail.com>
2025-07-24 09:51:52 +0800

    Update 009_BugQuotes.py

--- 009_BugQuotes.py
+++ 009_BugQuotes.py
@@ -5,7 +5,8 @@ quotes = [
     "Keep your face always toward the sunshine and shadows will fall behind you. - Walt Whitman",
     "A smooth sea never made a skilled sailor. - Franklin D. Roosevelt",
     "Even the darkest night will end and the sun will rise. - Victor Hugo",
-    "It is not the strongest of the species that survive, but the one most responsive to change . -Charles Darwin"
+    "It is not the strongest of the species that survive, but the one most responsive to change . -Charles Darwin",
+    "The top of one mountain is the bottom of the next, so keep climbing."
 ]
 
 print("random quote of the day buggy:\n")
